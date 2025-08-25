8f6c66a31dc3be23685286bab20027d22acfc3ea
Manikandan <mk475079@gamil.com>
2025-08-25 22:16:00 +0530

    Add theme support and UI components
    - Introduced a new  configuration file for component settings.
    - Removed  and added  for dependency management.
    - Updated  to include new dependencies for UI components and theming.
    - Enhanced global styles in  with new CSS variables and animations.
    - Implemented a  component for managing theme state.
    - Added  component for theme switching with dropdown menu.
    - Created reusable UI components including  and .
    - Refactored layout and page components to integrate new theming features.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
+import { ModeToggle } from "@/components/theme-toogle";
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <nav className="absolute top-4 right-4">
+          <ModeToggle/>
+        </nav>
         <div className="flex flex-col gap-2">
-
             <h1 className="text-2xl font-bold">Manikandan Saminathan</h1>
            <div className="flex justify-center">
            <h2 className="text-lg font-bold">Fungible developer</h2>
            </div>
-   
         </div>
       </main>
     </div>
